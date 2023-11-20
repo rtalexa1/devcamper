7d18954865853ad1511a3827491c8268901767d5
rtalexa1 <97422029+rtalexa1@users.noreply.github.com>
2023-11-20 06:46:35 -0600

    Create middleware

--- config/db.js
+++ config/db.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,clear
+    useUnifiedTopology: true
   });
 
-  console.log(`MongoDB connected: ${conn.connection.host}`);
+  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
 }
 
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -19,9 +28,18 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/v1/bootcamps", bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3000
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => {process.exit(1)});
+});
